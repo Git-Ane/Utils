57c0ad6c38143733da80586ab8be7a9e3087dc62
NathBo <nboyer200492@gmail.com>
2024-03-01 23:29:26 +0100

    c bon ca marche le setup_gitane.sh

--- setup_gitane.sh
+++ setup_gitane.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Path to your program
-PROGRAM_PATH="./Console/build/GitAneConsole"
+PROGRAM_PATH="$(pwd)/Console/build/GitAneConsole"
 
 # Desired command name
 COMMAND_NAME="gitane"
