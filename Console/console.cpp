9d7ad1b3a863636dce12818d0c295d16cc0c4f02
NathBo <nboyer200492@gmail.com>
2024-03-01 19:22:46 +0100

    hello message

---
+++ Console/console.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+
+
+
+int main(int argc, char* argv[]) {
+    if(argc>=2 && *argv[1] == *"hello"){
+        cout << "Hi from the Gitane Team !\n";
+    }
+    return 0;
+}
+
