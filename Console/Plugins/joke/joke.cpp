1d5d60a47e2fcde94727d60fb5c924386d141486
crdevio <clement.rouvroy80@gmail.com>
2024-03-02 21:03:53 +0100

    blague d'alice

--- Console/Plugins/joke/joke.cpp
+++ Console/Plugins/joke/joke.cpp
@@ -16,11 +16,16 @@ namespace GitAne{
             cout << "Quelles sont les pronoms d'un chinois sur Discord ?" << endl << "He/Yaaaa !" << endl;
         }
 
+        void blague_alice(vector<string>){
+            cout << "C'est un pingouin qui respire par les fesses." << endl << "Un jour il s'assoit et il meurt." << endl;
+        }
+
 
         void joke_plugin_loader(){
             initPlugin("joke");
             addCommand("jonathan",&jonhatan,"A joke about Jonathan",0,0);
             addCommand("chinese",&chinese,"A joke about chinese ppl",0,0);
+            addCommand("alice",&blague_alice,"blague de merde",0,0); // dsl pour son humour
         }
     }
 
