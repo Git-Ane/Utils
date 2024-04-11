0192331f08de39ef064527f338401a725a68a05f
NathBo <nboyer200492@gmail.com>
2024-04-11 20:27:25 +0200

    joke vincent

--- Console/Plugins/joke/joke.cpp
+++ Console/Plugins/joke/joke.cpp
@@ -22,12 +22,17 @@ namespace GitAne{
             cout << "C'est un pingouin qui respire par les fesses." << endl << "Un jour il s'assoit et il meurt." << endl;
         }
 
+        void blague_vincent(vector<string>) {
+            cout << "C'est un tétard qui croyait qui croyait qu'il était tôt mais enfaite il était tard" << endl;
+        }
+
 
         void joke_plugin_loader(){
             initPlugin("joke");
             addCommand("jonathan",&jonhatan,"A joke about Jonathan",0,0);
             addCommand("chinese",&chinese,"A joke about chinese ppl",0,0);
             addCommand("alice",&blague_alice,"blague de merde",0,0); // dsl pour son humour
+            addCommand("vincent",&blague_vincent,"hehe",0,0);
         }
     }
 
