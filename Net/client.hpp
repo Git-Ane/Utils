44f9fc6127fab75c5817269190e3d48b3562019a
crdevio <clement.rouvroy80@gmail.com>
2024-05-13 13:34:20 +0200

    token now public

--- Net/client.hpp
+++ Net/client.hpp
@@ -8,13 +8,14 @@ namespace GitAne{
 class NetClient {
     public:
         string server_url;
+        string token;
         NetClient(string s,string e, string m, bool reg);
         unordered_map<std::string, std::string> parseHTTPResponse(const std::string& httpResponse);
         unordered_map<std::string, std::string> body_to_args(const std::string& body);
         std::unordered_map<std::string, std::string> sendFile(string proj_name, string file_name, string file_content);
         std::unordered_map<std::string, std::string> receiveFile(string proj_name, string file_name);
     private:
-        string token;
+        
         string email;
     
     
