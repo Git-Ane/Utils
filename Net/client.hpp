bb43dce152be36dfdc0b69f7b344ba0bf0f15a3a
crdevio <clement.rouvroy80@gmail.com>
2024-05-13 12:37:55 +0200

    youpi

--- Net/client.hpp
+++ Net/client.hpp
@@ -9,6 +9,8 @@ class NetClient {
     public:
         string server_url;
         NetClient(string s,string e, string m){};
+        std::unordered_map<std::string, std::string> sendFile(string proj_name, string file_name, string file_content);
+        std::unordered_map<std::string, std::string> receiveFile(string proj_name, string file_name);
     private:
         string token;
         string email;
