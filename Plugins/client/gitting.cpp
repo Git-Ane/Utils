04c9c902a2ad2ba487739f075ed4b1b213ac9e21
NathBo <nboyer200492@gmail.com>
2024-05-16 15:58:49 +0200

    ca envoie tout

--- Plugins/client/gitting.cpp
+++ Plugins/client/gitting.cpp
@@ -1,7 +1,15 @@
 #include "gitting.hpp"
 
 using namespace GitAne;
+using namespace std;
 namespace fs = std::filesystem;
+
+
+string convert_filename(string filename){
+    std::string s = filename;
+    std::replace( s.begin(), s.end(), '/', '!');
+    return s;
+}
 
 
 void copy_to_server(string proj_name){
@@ -9,7 +17,8 @@ void copy_to_server(string proj_name){
     GitRepo repo = repo_find("");
     for (const auto& entry : fs::recursive_directory_iterator(repo.get_gitdir())){
         if(fs::is_regular_file(entry)){
-            string s = entry.path().string();
+            string s = convert_filename(entry.path().string());
+            cout << s << endl;
             cTest.sendFile(proj_name,s.substr(5,s.size()-5),get_file_content(entry));
             cout << s.substr(5,s.size()-5) << endl;
         }
